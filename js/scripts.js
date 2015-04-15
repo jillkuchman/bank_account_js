adffce3442bcfe63838f22afe093736f4e31ab7f
Jill Kuchman & Bryan Borgeson <student@epicodus.com>
2015-04-15 10:04:07 -0700

    Add current bank account balance text after account creation

--- js/scripts.js
+++ js/scripts.js
@@ -12,8 +12,20 @@ $(document).ready(function() {
     $('form#create-account').submit(function(event) {
         event.preventDefault();
 
+        var inputted_name = $('input#user-name').val();
+        var initial_deposit = parseFloat($('input#initial-deposit').val());
+
+        var newBankAccount = Object.create(BankAccount);
+        newBankAccount.username = inputted_name;
+        newBankAccount.deposit(initial_deposit);
+
+        // Shows deposit or withdraw form section
         $('#deposit-withdraw').show();
 
+        // Shows account balance section
+        $('#account-balance').show();
+
+        $('#current-balance').text(newBankAccount.balance);
 
         // Hides and resets sign up form section
         $('#splash-page').hide();
